2dd709eaa6fbbe1e8ab465089060247c325f70f9
Jorge Albarracín <sti.albarra@gmail.com>
2026-05-13 12:12:21 +0200

    Texto ejemplo Customer

--- backend/src/customers/dto/create-customer.dto.ts
+++ backend/src/customers/dto/create-customer.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class CreateCustomerDto {
+  @ApiProperty({ example: 'Antonio Aranda' })
   @IsString()
-  name!: string;
+  name: string;
 
+  @ApiProperty({ example: 'lococarioco@mongo.db' })
   @IsEmail()
-  email!: string;
+  email: string;
 
+  @ApiProperty({ example: '666 666 666' })
   @IsOptional()
   @IsString()
   phone?: string;
 
+  @ApiProperty({ example: 1 })
   @IsOptional()
   businessId?: number;
 }
