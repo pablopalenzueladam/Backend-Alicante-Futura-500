897417d48b1b170d307d0474ea14d89ae64bba6e
pablopalenzueladam <laptoptransfer777@gmail.com>
2026-06-04 10:37:25 +0200

    Delete Backend-Alicante-Futura-500-main/backend directory

--- Backend-Alicante-Futura-500-main/backend/src/auth/decorators/roles.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { CustomerRole } from '../../customers/customer.entity';
-
-export const ROLES_KEY = 'roles';
-
-export const Roles = (...roles: CustomerRole[]) => SetMetadata(ROLES_KEY, roles);
